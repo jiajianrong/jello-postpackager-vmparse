b6d02dfb10508076bad77db933c7937582f767a8
jiajianrong <jiajianrong@58.com>
2016-03-08 16:45:47 +0800

    exclude_parse_for_no_publish

--- index.js
+++ index.js
@@ -12,16 +12,20 @@ module.exports = function (ret, conf, settings, opt) {
         if (file.isHtmlLike && file.isViews) {
             
             // 替换#parse
-            var c = file._content.replace(r, function(all, target/*被parse的widget*/) {
+            var c = file._content.replace(r, function(all, target/*被parse的widget: '/widget/header/header.vm' */) {
                 
                 var url = '';
                 
                 // 再次遍历所有vm(isViews)，找到对应的widget vm
                 fis.util.map(ret.src, function(sub, f) {
-                    if ( f.isHtmlLike && f.isViews && target.indexOf(f.id) > -1 ) {
-                            
+                    if ( f.isHtmlLike && 
+                         f.isViews && 
+                         target.indexOf(f.id) > -1/*当前f match target: widget/header/header.vm*/ && 
+                         f.url.indexOf('WEB-INF') == -1/*当前f是发布给后端(带publish): /WEB-INF/views/widget/header/header.vm or /views/crm-m/widget/header/header.html */ ) {
+                        
                         url = '#parse(\'' + f.url + '\')';
                         !setFlag && (setFlag = true);
+                        
                         return true;
                     }
                 });
